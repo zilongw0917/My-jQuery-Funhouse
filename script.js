2b6aeb6014d7e8909bd1af0a132d458ab5f519c4
zilongw0917 <72420516+zilongw0917@users.noreply.github.com>
2020-10-20 17:37:26 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".Alcohol-lamp-cap").click(function() {
+    $(".Alcohol-lamp").slideToggle();
+});
+
+$(".doraemon-pocketl").click(function() {
+    $(".lots-of-items").slideToggle();
+});
+
+$(".curtain-remote-control").click(function() {
+    $(".curtain").slideToggle();
+});
